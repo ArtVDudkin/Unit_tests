edcde5e218604e12baa4ddade692b91e29b04478
Artem <kardan97@yandex.ru>
2023-11-30 20:27:51 +0700

    Vehicle tests added

--- Hometask_2/unit-tests-2 (Условие ДЗ)/Test/seminars/second/hw/VehicleTest.java
+++ Hometask_2/unit-tests-2 (Условие ДЗ)/Test/seminars/second/hw/VehicleTest.java
@@ -1,9 +1,80 @@
 package seminars.second.hw;
 
-import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class VehicleTest {
+
+    private Car car;
+    private Motorcycle motorcycle;
+
+    // Перед каждым тестом создаем экземпляр класса Car
+    @BeforeEach
+    void setUp() {
+        car = new Car("FHI","Subaru Forester",2009);
+        motorcycle = new Motorcycle("Honda","Shadow", 2003);
+    }
+
+    // После каждого теста "обнуляем" экземпляр класса Car, чтобы в следующий раз использовать новый экземпляр
+    @AfterEach
+    void tearDown() {
+        car = null;
+        motorcycle = null;
+    }
+
+    // - Проверить, что экземпляр объекта Car также является экземпляром транспортного средства (используя оператор instanceof).
+    @Test
+    @DisplayName("Проверка, что экземпляр объекта Car также является экземпляром Vehicle")
+    void isVehicle() {
+        assertTrue(car instanceof Vehicle);
+    }
+
+    // - Проверить, что объект Car создается с 4-мя колесами.
+    @Test
+    @DisplayName("Проверка, что объект Car создается с 4-мя колесами")
+    void checkCarWheels(){
+        assertEquals(4, car.getNumWheels());
+    }
+
+    // - Проверить, что объект Car развивает скорость 60 в режиме тестового вождения
+    @Test
+    @DisplayName("Проверка, что объект Car развивает скорость 60 в режиме тестового вождения")
+    void checkCarSpeedDrive(){
+        car.testDrive();
+        assertEquals(60, car.getSpeed());
+    }
+
+    // - Проверить, что в режиме парковки (сначала testDrive, потом park, т.е. эмуляция движения транспорта) машина останавливается (speed = 0)
+    @Test
+    @DisplayName("Проверка, что в режиме парковки (сначала testDrive, потом park, т.е. эмуляция движения транспорта) машина останавливается (speed = 0)")
+    void checkCarParking(){
+        car.testDrive();
+        car.park();
+        assertEquals(0, car.getSpeed());
+    }
+
+    // - Проверить, что объект Motorcycle создается с 2-мя колесами
+    @Test
+    @DisplayName("Проверка, что объект Motorcycle создается с 2-мя колесами")
+    void checkMotorcycleWheels(){
+        assertEquals(2, motorcycle.getNumWheels());
+    }
+
+    // - Проверить, что объект Motorcycle развивает скорость 75 в режиме тестового вождения
+    @Test
+    @DisplayName("Проверка, что объект Motorcycle развивает скорость 75 в режиме тестового вождения")
+    void checkMotorcycleDrive(){
+        motorcycle.testDrive();
+        assertEquals(75, motorcycle.getSpeed());
+    }
+
+    // - Проверить, что в режиме парковки (сначала testDrive, потом park, т.е. эмуляция движения транспорта) мотоцикл останавливается (speed = 0)
+    @Test
+    @DisplayName("Проверка, что в режиме парковки (сначала testDrive, потом park, т.е. эмуляция движения транспорта) мотоцикл останавливается (speed = 0)")
+    void checkMotorcycleParking(){
+        motorcycle.testDrive();
+        motorcycle.park();
+        assertEquals(0, motorcycle.getSpeed());
+    }
 
 }
